28a52e2d4978e35afbf9560ad010affaf6254e2e
DevClimber <wiktorzak.greg@gmail.com>
2020-09-16 17:21:11 +0100

    created custom-button.component and added to sign-in.component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FormInput from "../../components/form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./sign-in.styles.scss";
 
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
             required
           />
 
-          <input type="submit" value="Submiit Form" />
+          <CustomButton type="submit" value="Submit Form"> Sign in</CustomButton>
         </form>
       </div>
     );
